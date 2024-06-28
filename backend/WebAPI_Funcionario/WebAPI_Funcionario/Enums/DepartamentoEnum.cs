1cf5118171bcb39be7d6b1bf21c210cf2d5cf325
eduardoaalmeidaa <almeidaeduardu@gmail.com>
2024-06-28 12:34:07 -0300

    first commit

---
+++ backend/WebAPI_Funcionario/WebAPI_Funcionario/Enums/DepartamentoEnum.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json.Serialization;
+
+namespace WebAPI_Funcionario.Enums
+{
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum DepartamentoEnum
+    {
+        Administrativo,
+        Comercial,
+        Compras,
+        Contabilidade,
+        Financeiro,
+        Juridico,
+        Logistica,
+        Marketing,
+        RH,
+        Suporte,
+        TI
+    }
+}
---
+++ backend/WebAPI_Funcionario/WebAPI_Funcionario/Enums/TurnoEnum.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace WebAPI_Funcionario.Enums
+{
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum TurnoEnum
+    {
+        Manha,
+        Tarde,
+        Noite
+    }
+}
